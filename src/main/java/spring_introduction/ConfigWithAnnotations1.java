66108e31a5cc3bdfa7127887c396b47e13e37b49
SerhiiSkakun <skakun1978@gmail.com>
2023-09-16 08:57:51 +0300

    first commit

---
+++ src/main/java/spring_introduction/ConfigWithAnnotations1.java
@@ -0,0 +1,17 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ConfigWithAnnotations1 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Person person = context.getBean("person", Person.class);
+        person.callYourPet();
+
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
+
+        context.close();
+    }
+}
